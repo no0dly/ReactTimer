d2e6f439cf4219a97af379d134a23f04c9811bd0
Vadim Derman <thenoodly@gmail.com>
2016-10-15 22:50:12 -0700

    First commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = (props) => {
+    return (
+        <div className="top-bar top-nav">
+            <div className="top-bar-left">
+                <ul className="dropdown menu color-black" data-dropdown-menu>
+                    <li className="menu-text nav-title">React timer app</li>
+                    <li>
+                        <IndexLink to="/" activeClassName="active">Timer</IndexLink>
+                    </li>
+                    <li>
+                        <Link to="/" activeClassName="active">Countdown</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="top-bar-right">
+                <ul className="menu color-black">
+                    <li className="nav-author">
+                        Created by
+                    </li>
+                    <li>
+                        <a target="_blank" className="nav-author__link" href="https://github.com/no0dly">noodly</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+};
+
+module.exports = Nav;
